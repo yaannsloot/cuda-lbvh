c8858dce74bf387d2eddf3b67cc638d74b923101
yaannsloot <iansloat537@gmail.com>
2026-04-27 00:10:20 -0500

    debugging and formatting

--- src/bvh.h
+++ src/bvh.h
@@ -68,11 +68,11 @@ struct bvh {
     buf_gpu<int> nor_indices;
 };
 
-//empty for now but will be needed later
-struct build_state{
+struct build_state {
 
-    bvh_node* nodes;
-    uint32_t* cluster_indicies;
+    bvh_node *nodes;
+    uint32_t *cluster_indicies;
+    uint32_t *parent_indicies;
     uint32_t prim_count;
-    uint32_t cluster_count;
+    uint32_t *cluster_count;
 };
